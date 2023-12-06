a1993cbc0a1c45f6c5531e7fb5df96406827ea0e
almeidas-tatiane <almeidas.tatiane@gmail.com>
2023-12-06 12:45:36 -0300

    Adding crocodiles_SmokeTest simulation

---
+++ src/crocodiles/simulations/crocodiles_SmokeTest.js
@@ -0,0 +1,27 @@
+import { group } from 'k6';
+import Crocodiles from '../requests/crocodiles.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export const options = {
+  vus: 1, 
+  duration: '3s',
+
+  thresholds: {
+    http_req_failed: ['rate < 0.01'],
+    http_req_duration: ['p(95) < 900']
+  }
+};
+
+export default function () {
+  const crocodiles = new Crocodiles();
+
+  group('smoke test', () => {
+    crocodiles.getAllCrocodiles();
+  })
+}
+
+export function handleSummary(data) {
+  return {
+    "reports/crocodiles_SmokeTest.html": htmlReport(data),
+  };
+}
