e3a2ee222bea6006082a037b6388d48aee7d5542
almeidas-tatiane <almeidas.tatiane@gmail.com>
2023-12-06 17:19:30 -0300

    Updating crocodiles_SmokeTest.js

--- src/crocodiles/simulations/crocodiles_SmokeTest.js
+++ src/crocodiles/simulations/crocodiles_SmokeTest.js
@@ -1,6 +1,6 @@
 import { group } from 'k6';
 import Crocodiles from '../requests/crocodiles.js';
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+// import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
   vus: 1,
@@ -9,19 +9,22 @@ export const options = {
   thresholds: {
     http_req_failed: ['rate < 0.01'],
     http_req_duration: ['p(95) < 900'],
-  },
+  }
+ 
 };
 
 export default function () {
   const crocodiles = new Crocodiles();
 
-  group('smoke test', () => {
+  group('GetAllCrocodiles', () => {
     crocodiles.getAllCrocodiles();
-  });
+    
+  }
+  );
 }
 
-export function handleSummary(data) {
-  return {
-    "reports/crocodiles_SmokeTest.html": htmlReport(data),
-  };
-}
+// export function handleSummary(data) {
+//   return {
+//     "reports/crocodiles_SmokeTest.html": htmlReport(data),
+//   };
+// }
