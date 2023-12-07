114c47ea7ea8960856a3ababf091670c951c8019
almeidas-tatiane <almeidas.tatiane@gmail.com>
2023-12-07 11:32:09 -0300

    Updating script with imports

--- src/crocodiles/simulations/crocodiles_LoadTest_AWS.js
+++ src/crocodiles/simulations/crocodiles_LoadTest_AWS.js
@@ -1,3 +1,5 @@
+import http from 'k6/http';
+import { check } from 'k6';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 // Verify buildspec.yml to execute tests at AWS using codebuild.
