0d5196ee72cda100e19aebf5b861ae3961c7c318
almeidas-tatiane <almeidas.tatiane@gmail.com>
2023-12-11 16:50:21 -0300

    Updating crocodilesClass.js

--- src/crocodiles/requests/crocodilesClass.js
+++ src/crocodiles/requests/crocodilesClass.js
@@ -2,42 +2,37 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { getUrlByKey } from '../../utils/urlProperties.js';
 import { SharedArray } from "k6/data";
-import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+
+const data = new SharedArray('Leitura do json', function() {
+    return JSON.parse(open('../../data/json/crocodilesById.json')).crocodiles;
+});
 
 export default class Crocodiles {
 
     getAllCrocodiles() {
-
         const sentHeadersAll = {
             headers: {
                 'Content-Type': 'application/json'
             }
         };
 
-        const response = http.get(`${getUrlByKey('api')}/public/crocodiles/`, sentHeadersAll);
-
+        const response = http.get(`${getUrlByKey('api')}/public/crocodiles/`, sentHeadersAll, {tags:{name: 'getAllCrocodiles'}});
 
         check(response, {
             'Status code 200': (resp) => resp.status === 200,
         });
     }
 
-    getCrocodilesbyID(){
-
+    getCrocodilesbyID() {
         const sentHeadersById = {
             headers: {
                 'Content-Type': 'application/json'
             }
         };
 
-        const csvData = new SharedArray('Leitura do csv', function(){
-            return papaparse.parse(open('../../data/csv/crocodilesById.csv'), {header: true}).data;
-        });
+        const id = data[Math.floor(Math.random() * data.length)].id;
 
-        const id = csvData[Math.floor(Math.random() * csvData.length)].id;
-
-
-        const response = http.get(`${getUrlByKey('api')}/public/crocodiles/${id}`, sentHeadersById);
+        const response = http.get(`${getUrlByKey('api')}/public/crocodiles/${id}`, sentHeadersById, {tags: {name: 'getCrocodilesbyID'}});
 
         check(response, {
             'Status code 200': (resp) => resp.status === 200,
