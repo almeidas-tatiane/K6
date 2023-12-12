bcb2bf8b8cb76a15d97a57b72c219db9d24e71de
almeidas-tatiane <almeidas.tatiane@gmail.com>
2023-12-11 21:47:45 -0300

    Adding users_SmokeTest.js

---
+++ src/user/simulations/users_SmokeTest.js
@@ -0,0 +1,22 @@
+import { group } from 'k6';
+import Users from '../requests/userClass.js';
+
+export const options = {
+    vus:  1,
+    duration: '3s',
+
+    thresholds: {
+        http_req_failed: ['rate < 0.01'],
+        http_req_duration: ['p(90) < 900'],
+    }
+};
+
+export default function () {
+    const users = new Users();
+
+    group('createNewUser', () => {
+      users.createNewUser();
+    }
+    );
+}
+
