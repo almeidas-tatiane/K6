c4a26025421fb396b641a1e2f33b318d9ad5dbd9
almeidas-tatiane <almeidas.tatiane@gmail.com>
2023-12-12 14:16:55 -0300

    Adding createNewCrocodile()

--- src/crocodiles/requests/crocodilesClass.js
+++ src/crocodiles/requests/crocodilesClass.js
@@ -2,9 +2,14 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { getUrlByKey } from '../../utils/urlProperties.js';
 import { SharedArray } from "k6/data";
+import { login } from '../../../user/requests/userClass.js';
 
 const data = new SharedArray('Leitura do json', function() {
     return JSON.parse(open('../../data/json/crocodilesById.json')).crocodiles;
+});
+
+const dataNewCrocodile = new SharedArray('Leitura do json', function() {
+    return JSON.parse(open('../../data/json/newCrocodiles.json')).crocodiles;
 });
 
 export default class Crocodiles {
@@ -37,5 +42,30 @@ export default class Crocodiles {
         check(response, {
             'Status code 200': (resp) => resp.status === 200,
         });
+    };
+
+    createNewCrocodile(){
+
+        const sentHeadersNewCrocodile = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const bodyNewCrocodile = {
+            name: dataNewCrocodile[Math.floor(Math.random() * dataNewCrocodile.length)].name,
+            sex:  dataNewCrocodile[Math.floor(Math.random() * dataNewCrocodile.length)].sex,
+            date_of_birth: dataNewCrocodile[Math.floor(Math.random() * dataNewCrocodile.length)].date_of_birth
+        };
+
+        login();
+        const responseNewCrocodile = http.post(`${getUrlByKey('api')}/my/crocodiles/`, bodyNewCrocodile, { headers: sentHeadersNewCrocodile }  );
+        console.log(bodyNewCrocodile);
+        console.log(responseNewCrocodile);
+        
+        check(responseLogin, {
+            'Status code 201': (resp) => resp.status === 201,
+        });
+
     }
 }
